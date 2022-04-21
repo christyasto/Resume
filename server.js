c7c34d5e24bf7c7c216383a1c6e15541989167c6
Christyasto Priyonggo Pambudi <christyasto.chito@gmail.com>
2022-04-21 20:20:28 +0700

    Added handleDisconnect()
    to solve db server restart or whatever db connection issue...

--- server.js
+++ server.js
@@ -12,21 +12,36 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 
-// Connection Details
-const connection = mysql.createConnection({
-    host: host,
-    user: user,
-    password: pass,
-    database: db,
-    multipleStatements: true
-})
+var connection;
+function handleDisconnect() {
+    // Connection Details
+    connection = mysql.createConnection({
+        host: host,
+        user: user,
+        password: pass,
+        database: db,
+        multipleStatements: true
+    })
 
-// Force mysql to keep connection alive every 3 hours
-setInterval(function () {
-    connection.query('SELECT 1');
-    console.log("tick");
-}, 1 * 3600 * 1000);
+    connection.connect(function (err) {                 // The server is either down
+        if (err) {                                      // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);         // We introduce a delay before attempting to reconnect,
+        }                                               // to avoid a hot loop, and to allow our node script to
+    });                                                 // process asynchronous requests in the meantime.
 
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {  // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    })
+}
+
+handleDisconnect();
 // View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
