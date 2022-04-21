2f433d692cffa17030a48578d9b2d85c7038dd42
Christyasto Priyonggo Pambudi <christyasto.chito@gmail.com>
2022-04-21 21:49:09 +0700

    Added Favicon

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function handleDisconnect() {
     connection.connect(function (err) {                 // The server is either down
         if (err) {                                      // or restarting (takes a while sometimes).
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);         // We introduce a delay before attempting to reconnect,
+            setTimeout(handleDisconnect, 2*1000);         // We introduce a delay before attempting to reconnect,
         }                                               // to avoid a hot loop, and to allow our node script to
     });                                                 // process asynchronous requests in the meantime.
 
@@ -33,9 +33,10 @@ function handleDisconnect() {
     connection.on('error', function (err) {
         console.log('db error', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {  // Connection to the MySQL server is usually
+            console.log("Connection lost, recalling handleDisconnect..."); 
             handleDisconnect();                         // lost due to either server restart, or a
         } else {                                        // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
+            throw err.code;                                  // server variable configures this)
         }
     })
 }
